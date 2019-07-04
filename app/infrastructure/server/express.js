2b9a68874c766accbf2c89e26bc1e3aea20152d1
pepese <tanakakns@MacBook.local>
2019-07-05 08:14:53 +0900

    update

--- app/infrastructure/server/express.js
+++ app/infrastructure/server/express.js
@@ -4,14 +4,20 @@ const express = require("express");
 const app = express();
 const router = require("../../interface/router");
 const helmet = require("helmet");
+const logger = require("../../logger");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use("/", router);
-app.use((req, res) => {
-  res.status(404);
-  res.json({ message: "Not Found." });
+// 500
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error." }).end();
+});
+// 404
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not Found." }).end();
 });
 app.set("port", "3000");
 
