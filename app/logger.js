e386b7054856d86b605a95d37dbaba75439e8a2c
pepese <tanakakns@MacBook.local>
2019-07-17 08:18:42 +0900

    updat3e

--- app/logger.js
+++ app/logger.js
@@ -4,7 +4,18 @@ const winston = require('winston');
 const httpContext = require('express-http-context');
 
 const winstonLogger = winston.createLogger({
-  format: winston.format.json(),
+  levels: {
+    'error': 0,
+    'warn': 1,
+    'verbose': 2,
+    'info': 3,
+    'debug': 4,
+    'silly': 5
+  },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [new winston.transports.Console()],
 });
 
