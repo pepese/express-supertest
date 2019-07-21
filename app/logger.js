a20be8f67234a27619b004e7d3e5e0cbd1f9d9d7
pepese <tanakakns@MacBook.local>
2019-07-22 08:22:10 +0900

    update

--- app/logger.js
+++ app/logger.js
@@ -19,47 +19,6 @@ const winstonLogger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
-// // Wrap Winston logger to print reqId in each log
-// var formatMessage = function(message) {
-//   var reqId = httpContext.get('reqId');
-//   // if (reqId) {
-//   //   message.reqId = reqId;
-//   // }
-//   // return message;
-//   if (reqId) {
-//     return {
-//       reqId: reqId,
-//       message: message
-//     };
-//   } else {
-//     return message;
-//   }
-// };
-
-// var logger = {
-//   log: function(level, message, meta = null) {
-//     winstonLogger.log(level, formatMessage(message), meta);
-//   },
-//   error: function(message) {
-//     winstonLogger.error(formatMessage(message));
-//   },
-//   warn: function(message) {
-//     winstonLogger.warn(formatMessage(message));
-//   },
-//   verbose: function(message) {
-//     winstonLogger.verbose(formatMessage(message));
-//   },
-//   info: function(message) {
-//     winstonLogger.info(formatMessage(message));
-//   },
-//   debug: function(message) {
-//     winstonLogger.debug(formatMessage(message));
-//   },
-//   silly: function(message) {
-//     winstonLogger.silly(formatMessage(message));
-//   },
-// };
-
 exports.getLogger = () => {
   let logger = httpContext.get('logger');
   if (!logger) {
